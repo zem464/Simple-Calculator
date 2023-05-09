dc6ccab2cb510f630de499eef94bdad24863fb0e
Iris Membrere <irismembrere@gmail.com>
2023-05-09 21:32:15 +0800

    Pseudocodes

---
+++ Simple-Calculator.py
@@ -0,0 +1,81 @@
+# Define the operations to be used
+
+# Print intro or greetings
+
+# Ask how many numbers are to be calculated
+
+# For 2 inputs
+
+# Show operations
+
+# Use while loop
+
+# Take the operations to be used by the users
+
+# Check the input 
+
+# Use try-except handling
+
+# If addition
+
+# If subtraction
+
+# If multiplication
+
+# If division
+
+# Then ask for more calculations
+
+# Continue if there are more calculations
+
+# If input is invalid
+
+# For 3 inputs
+
+# Show operations
+
+# Use while loop
+
+# Take the operations to be used by the users
+
+# Check the input
+
+# use try-except handling
+
+# If both addition
+
+# If addition and subtraction
+
+# If addition and multiplication
+
+# If addition and division
+
+# If subtraction and addition
+            
+# If both subtraction
+
+# If subtraction and multiplication
+            
+# If multiplication and addition
+            
+# If multiplication and subtraction
+
+# If both multiplication
+
+# If multiplication and division
+            
+# If division and addition
+            
+# If division and subtraction
+
+# If division and multiplication
+            
+# If both division
+
+# Then ask for more calculations
+
+# Break if no other calculations
+
+# If input is invalid
+
+# Give choices, if input is not in the choices
