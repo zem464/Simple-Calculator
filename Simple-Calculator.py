80f353653ffda7e795b8e8c9cc83e7732740ab56
Iris Membrere <irismembrere@gmail.com>
2023-05-09 21:36:11 +0800

    Defining functions for the operations

--- Simple-Calculator.py
+++ Simple-Calculator.py
@@ -1,4 +1,12 @@
 # Define the operations to be used
+def add(num1, num2):
+    return num1 + num2
+def subtract(num1, num2):
+    return num1 - num2
+def multiply(num1, num2):
+    return num1 * num2
+def divide(num1, num2):
+    return num1 / num2
 
 # Print intro or greetings
 
